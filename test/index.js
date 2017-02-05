f38a58a3ffad94fa8f5bb1153140d4102ded1e51
rudylacrete <rudylacrete@gmail.com>
2017-02-06 00:58:19 +0400

    handle correctly port open event + fixed existing writting + added commands

--- test/index.js
+++ test/index.js
@@ -1,3 +1,13 @@
 const Driver = require('..');
 
-const d = new Driver('/dev/ttyACM0');
+const d = new Driver('/dev/ttyAMA0');
+d.on('error', (error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+d.checkModuleIsAlive().then(() => console.log('Alive ......'))
+.then(() => d.dumpModuleInformations())
+.then(() => d.sendBytes(Buffer.from('Hello world')))
+.then(() => console.log('all done ...'))
+.catch(console.error.bind(console));
